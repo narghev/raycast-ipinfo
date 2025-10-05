fb98f4b9b42f011a6c5c6eca7c616e0b7e31902e
narghev <narghev@protonmail.com>
2025-10-05 20:23:10 +0400

    show IP country name and flag

--- src/components/ip-details.tsx
+++ src/components/ip-details.tsx
@@ -1,25 +1,29 @@
 import { Detail } from "@raycast/api";
 import { IPBogon, IPinfo, IPinfoLite } from "node-ipinfo/dist/src/common";
 import { BogonIP } from "./bogon-ip";
+import { getFlagEmoji } from "../utils/country-emoji";
 
 interface IpDetailsProps {
   ipInfo: IPinfoLite | IPinfo | IPBogon | null;
   isLoading: boolean;
 }
 
-export const IpDetails = ({ ipInfo, isLoading }: IpDetailsProps) => {
-  if (ipInfo && "bogon" in ipInfo && ipInfo.bogon) {
-    return <BogonIP ipInfo={ipInfo as IPBogon} />;
-  }
-
-  console.log(ipInfo, isLoading);
-
-  const markdown = `
+const constructMarkdown = (ipInfo: IPinfoLite | IPinfo) => `
+## Your IP Information
+---
 # **${ipInfo?.ip}**
+## ${ipInfo.country} ${getFlagEmoji(ipInfo.countryCode)}
 \`\`\`json
 ${JSON.stringify(ipInfo, null, 2)}
 \`\`\`
 `;
 
-  return <Detail markdown={!isLoading ? markdown : ""} isLoading={isLoading} />;
+export const IpDetails = ({ ipInfo, isLoading }: IpDetailsProps) => {
+  if (ipInfo && "bogon" in ipInfo && ipInfo.bogon) {
+    return <BogonIP ipInfo={ipInfo as IPBogon} />;
+  }
+
+  const notBogonIp = ipInfo as IPinfoLite | IPinfo;
+
+  return <Detail markdown={!isLoading && notBogonIp ? constructMarkdown(notBogonIp) : ""} isLoading={isLoading} />;
 };
